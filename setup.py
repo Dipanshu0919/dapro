76c82c19f67e3b21eaf1b3e4a7b8510bbfd87e6c
Dipanshu Ashok Agarwal <hu1243009@sjchs.edu.in>
2025-06-01 18:50:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dapro',
-    version='0.1.4',
+    version='0.0.0',
     description='A library to simplify eval and file handling in Telegram bots.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Dipanshu',
     author_email='dipanshu0919@gmail.com',
     url='https://github.com/Dipanshu0919/dapro',
-    packages=find_packages(where='dapro'),
+    packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
